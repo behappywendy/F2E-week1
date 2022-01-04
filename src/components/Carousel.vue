3e4dec2a352dcfc2101d127b7afab5e35df63728
wendy.liu <aaa3520732@gmail.com>
2022-01-04 14:35:40 +0800

    feat: 輪播圖

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,14 +1,8 @@
 <template>
   <swiper class="slide" :slides-per-view="3" :space-between="50" @swiper="onSwiper" @slideChange="onSlideChange">
-    <swiper-slide><img src="./../assets/image/card1.png" alt="" /></swiper-slide>
-    <swiper-slide><img src="./../assets/image/card1.png" alt="" /></swiper-slide>
-    <swiper-slide><img src="./../assets/image/card1.png" alt="" /></swiper-slide>
-    <swiper-slide><img src="./../assets/image/card1.png" alt="" /></swiper-slide>
-    <swiper-slide><img src="./../assets/image/card1.png" alt="" /></swiper-slide>
-    <swiper-slide><img src="./../assets/image/card1.png" alt="" /></swiper-slide>
-    <swiper-slide><img src="./../assets/image/card1.png" alt="" /></swiper-slide>
-    <swiper-slide><img src="./../assets/image/card1.png" alt="" /></swiper-slide>
-    <swiper-slide><img src="./../assets/image/card1.png" alt="" /></swiper-slide>
+    <swiper-slide v-for="(image, index) in allImage" :key="index">
+      <img :src="image" alt="" />
+    </swiper-slide>
   </swiper>
 </template>
 <script>
@@ -17,6 +11,11 @@ import { SwiperSlide } from "swiper/vue/swiper-slide.js";
 import "swiper/swiper.min.css";
 export default {
   name: "Carousel",
+  props: {
+    allImage: {
+      type: Array,
+    },
+  },
   components: {
     Swiper,
     SwiperSlide,
@@ -24,13 +23,14 @@ export default {
   data() {
     return {};
   },
+  methods: {
+    clickImage() {
+      console.log(this.allImage);
+    },
+  },
   setup() {
-    const onSwiper = () => {
-      // console.log(swiper);
-    };
-    const onSlideChange = () => {
-      // console.log("slide change");
-    };
+    const onSwiper = () => {};
+    const onSlideChange = () => {};
     return {
       onSwiper,
       onSlideChange,
@@ -41,9 +41,12 @@ export default {
 <style scoped lang="scss">
 .slide .swiper-slide {
   width: 128px !important;
+  height: 100px;
   margin-right: 8px !important;
   img {
     width: 100%;
+    height: 100%;
+    object-fit: fill;
   }
 }
 </style>
